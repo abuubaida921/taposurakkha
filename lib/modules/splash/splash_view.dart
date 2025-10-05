e152548e95206342633e9be63d8a17203990f82e
Abu Ubaida <ubaidanazia@gmail.com>
2025-10-05 22:59:34 +0600

    Enhancement

--- lib/modules/splash/splash_view.dart
+++ lib/modules/splash/splash_view.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:taposurakkha/app/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,16 +20,234 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with TickerProviderStateMixin {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
 
   @override
   void initState() {
-    redirectUser();
     super.initState();
+    initPlatformState();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
+  Future<void> initPlatformState() async {
+    var deviceData = <String, dynamic>{};
+    try {
+      if (kIsWeb) {
+        deviceData = _readWebBrowserInfo(await deviceInfoPlugin.webBrowserInfo);
+      } else {
+        deviceData = switch (defaultTargetPlatform) {
+          TargetPlatform.android => _readAndroidBuildData(await deviceInfoPlugin.androidInfo),
+          TargetPlatform.iOS => _readIosDeviceInfo(await deviceInfoPlugin.iosInfo),
+          TargetPlatform.linux => _readLinuxDeviceInfo(await deviceInfoPlugin.linuxInfo),
+          TargetPlatform.windows => _readWindowsDeviceInfo(await deviceInfoPlugin.windowsInfo),
+          TargetPlatform.macOS => _readMacOsDeviceInfo(await deviceInfoPlugin.macOsInfo),
+          TargetPlatform.fuchsia => <String, dynamic>{'Error:': 'Fuchsia platform isn\'t supported'},
+        };
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{'Error:': 'Failed to get platform version.'};
+    }
+    if (!mounted) return;
+    setState(() {
+      _deviceData = deviceData;
+    });
+    fetchData();
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'name': build.name,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+      'isLowRamDevice': build.isLowRamDevice,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'modelName': data.modelName,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'isiOSAppOnMac': data.isiOSAppOnMac,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+
+  Map<String, dynamic> _readLinuxDeviceInfo(LinuxDeviceInfo data) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'name': data.name,
+      'version': data.version,
+      'id': data.id,
+      'idLike': data.idLike,
+      'versionCodename': data.versionCodename,
+      'versionId': data.versionId,
+      'prettyName': data.prettyName,
+      'buildId': data.buildId,
+      'variant': data.variant,
+      'variantId': data.variantId,
+      'machineId': data.machineId,
+    };
+  }
+
+  Map<String, dynamic> _readWebBrowserInfo(WebBrowserInfo data) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'browserName': data.browserName.name,
+      'appCodeName': data.appCodeName,
+      'appName': data.appName,
+      'appVersion': data.appVersion,
+      'deviceMemory': data.deviceMemory,
+      'language': data.language,
+      'languages': data.languages,
+      'platform': data.platform,
+      'product': data.product,
+      'productSub': data.productSub,
+      'userAgent': data.userAgent,
+      'vendor': data.vendor,
+      'vendorSub': data.vendorSub,
+      'hardwareConcurrency': data.hardwareConcurrency,
+      'maxTouchPoints': data.maxTouchPoints,
+    };
+  }
+
+  Map<String, dynamic> _readMacOsDeviceInfo(MacOsDeviceInfo data) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'computerName': data.computerName,
+      'hostName': data.hostName,
+      'arch': data.arch,
+      'model': data.model,
+      'modelName': data.modelName,
+      'kernelVersion': data.kernelVersion,
+      'majorVersion': data.majorVersion,
+      'minorVersion': data.minorVersion,
+      'patchVersion': data.patchVersion,
+      'osRelease': data.osRelease,
+      'activeCPUs': data.activeCPUs,
+      'memorySize': data.memorySize,
+      'cpuFrequency': data.cpuFrequency,
+      'systemGUID': data.systemGUID,
+    };
+  }
+
+  Map<String, dynamic> _readWindowsDeviceInfo(WindowsDeviceInfo data) {
+    return <String, dynamic>{
+      'uuid': '123e4567-e89b-13d2-a556-436614173003',
+      'numberOfCores': data.numberOfCores,
+      'computerName': data.computerName,
+      'systemMemoryInMegabytes': data.systemMemoryInMegabytes,
+      'userName': data.userName,
+      'majorVersion': data.majorVersion,
+      'minorVersion': data.minorVersion,
+      'buildNumber': data.buildNumber,
+      'platformId': data.platformId,
+      'csdVersion': data.csdVersion,
+      'servicePackMajor': data.servicePackMajor,
+      'servicePackMinor': data.servicePackMinor,
+      'suitMask': data.suitMask,
+      'productType': data.productType,
+      'reserved': data.reserved,
+      'buildLab': data.buildLab,
+      'buildLabEx': data.buildLabEx,
+      'digitalProductId': data.digitalProductId,
+      'displayVersion': data.displayVersion,
+      'editionId': data.editionId,
+      'installDate': data.installDate,
+      'productId': data.productId,
+      'productName': data.productName,
+      'registeredOwner': data.registeredOwner,
+      'releaseId': data.releaseId,
+      'deviceId': data.deviceId,
+    };
+  }
+
+  Future<void> fetchData() async {
+    await Future.delayed(const Duration(seconds: 1));
+    try {
+      var response = await http.post(
+        Uri.parse("https://demo.abuubaida921.com/get_app_details.php"),
+        body: jsonEncode(_deviceData),
+        headers: {'Content-Type': 'application/json'},
+      );
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        if (data['data']['status'] == 'active') {
+          Get.offAllNamed(AppRoutes.dashboard);
+        } else {
+          showError("App is not active, Contact with Developer", false);
+        }
+      } else {
+        showError("Failed to fetch data", true);
+      }
+    } catch (e) {
+      showError("Something went wrong! $e", true);
+    }
+  }
+
+  void showError(String message, bool isError) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          title: Text(message, style: TextStyle(fontSize: 16)),
+          content: isError
+              ? Text('Please try again', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 20))
+              : Icon(Icons.error, color: Colors.red, size: 48),
+          actions: [
+            InkWell(
+              onTap: () async {
+                exit(0);
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.white,
+                ),
+                child: Text("Exit", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -45,12 +269,5 @@ class _SplashScreenState extends State<SplashScreen>
         ),
       ),
     );
-  }
-
-  void redirectUser() async {
-
-    Future.delayed(Duration(seconds: 1), () {
-      Get.offAllNamed(AppRoutes.dashboard);
-    });
   }
 }
